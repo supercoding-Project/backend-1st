a4fa0d1fdf9a0994b716e88c3b6d12d39dc83278
Lee hana <hn110178@naver.com>
2025-02-13 01:31:30 +0900

    like-api

--- src/main/java/com/github/firstproject/likes/entity/LikesEntity.java
+++ src/main/java/com/github/firstproject/likes/entity/LikesEntity.java
@@ -1,6 +1,5 @@
 package com.github.firstproject.likes.entity;
 
-
 import com.github.firstproject.auth.entity.UserEntity;
 import com.github.firstproject.comment.entity.Comment;
 import jakarta.persistence.*;
